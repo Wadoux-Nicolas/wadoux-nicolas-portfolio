97b6bd2f517e0fd6a8a3e10f5db1887dff48fb1c
Nicolas W <nwadoux@myges.fr>
2024-03-14 13:53:16 +0100

    add hello component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,7 @@
-import {useTranslations} from 'next-intl';
+import Hello from "@/components/hello";
 
 export default function Page() {
-  const t = useTranslations();
-  return <h1>{t('hello')}</h1>;
+  return (
+      <Hello/>
+  )
 }
